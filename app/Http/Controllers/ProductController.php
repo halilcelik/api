681de9b5a05491a86d2ef11b22c376c00fb9ecb2
Halil <Halil2014hn@hotmail.com>
2019-08-16 23:46:29 +0300

    Product update işlemi yapıldı

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
+use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
+
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -34,11 +43,23 @@ class ProductController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return array
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = new Product;
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->save();
+
+
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
+
     }
 
     /**
@@ -70,11 +91,20 @@ class ProductController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Model\Product  $product
-     * @return \Illuminate\Http\Response
+     * @return Product
      */
     public function update(Request $request, Product $product)
     {
-        //
+      $request['detail'] = $request->description;
+
+      unset($request['description']);
+
+      $product->update($request->all());
+
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
+
     }
 
     /**
--- app/Model/Product.php
+++ app/Model/Product.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+
+    protected $fillable = [
+        'name','detail','stock','price','discount'
+    ];
     public function reviews(){
 
         return $this->hasMany(Review::class);
