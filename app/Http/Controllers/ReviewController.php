7018ec7d08451df76dce7a88ab683e8d4bd82012
Halil <Halil2014hn@hotmail.com>
2019-08-16 16:23:23 +0300

    Review Resource & Product sayfalandırma yapıldı

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ReviewResource;
+use App\Model\Product;
 use App\Model\Review;
 use Illuminate\Http\Request;
 
@@ -10,11 +12,12 @@ class ReviewController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
-    public function index()
+    public function index(Product $product)
     {
-        //
+
+        return ReviewResource::collection($product->reviews);
     }
 
     /**
