6c2a4363bf61a8cebf5275cd1515f8873bebf6af
Halil <Halil2014hn@hotmail.com>
2019-08-16 14:11:46 +0300

    ProductResource oluşturuldu

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
 
@@ -10,11 +11,12 @@ class ProductController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Product[]|\Illuminate\Database\Eloquent\Collection
      */
     public function index()
     {
-        //
+        return Product::all();
+
     }
 
     /**
@@ -42,11 +44,13 @@ class ProductController extends Controller
      * Display the specified resource.
      *
      * @param  \App\Model\Product  $product
-     * @return \Illuminate\Http\Response
+     * @return ProductResource
      */
     public function show(Product $product)
     {
-        //
+
+
+        return new ProductResource($product);
     }
 
     /**
