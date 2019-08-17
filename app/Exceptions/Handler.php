aeef2f56298a497e2e0cabfdfe449f7ec18bee58
Halil <Halil2014hn@hotmail.com>
2019-08-17 13:59:24 +0300

    Yönlendirme hata uyarıları düzeltildi

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions;
 
 use Exception;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -46,6 +49,27 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+
+        if ($request->expectsJson()) {
+            if ($exception instanceof ModelNotFoundException){
+
+                return response()->json([
+                    'errors' => 'Product model bulunamadı'
+                ],Response::HTTP_NOT_FOUND);
+
+            }
+            
+            if ($exception instanceof NotFoundHttpException){
+
+                return response()->json([
+                    'errors' => 'Yönlendirme hatası'
+                ],Response::HTTP_NOT_FOUND);
+            }
+
+        }
+
+
+       // dd($exception);
         return parent::render($request, $exception);
     }
 }
