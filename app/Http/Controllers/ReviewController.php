dda65142cde28fb8e1431b7decd10e513bc1be71
Halil <Halil2014hn@hotmail.com>
2019-08-19 00:43:10 +0300

    Silme Yapıldı

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -75,11 +75,18 @@ class ReviewController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Model\Review  $review
-     * @return \Illuminate\Http\Response
+     * @return Review
      */
-    public function update(Request $request, Review $review)
+    public function update(Request $request, Product $product, Review $review)
     {
-        //
+        $review->update($request->all());
+
+        return response([
+            'data' => new ReviewResource($review)
+
+        ],Response::HTTP_CREATED);
+
+
     }
 
     /**
@@ -88,8 +95,10 @@ class ReviewController extends Controller
      * @param  \App\Model\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Review $review)
+    public function destroy(Product $product, Review $review)
     {
-        //
+        $review->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
